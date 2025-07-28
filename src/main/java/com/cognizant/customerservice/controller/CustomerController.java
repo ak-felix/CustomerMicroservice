34413708d9b3be612d171ca5025b61ccd8b2be65
ak-felix <akfelix00@gmail.com>
2025-07-28 11:06:39 +0530

    Revert "Update field names in CustomerController"
    This reverts commit 87b6f3948b1e5f96698f93082bacc428bef506f1.

--- src/main/java/com/cognizant/customerservice/controller/CustomerController.java
+++ src/main/java/com/cognizant/customerservice/controller/CustomerController.java
@@ -28,10 +28,10 @@ import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 public class CustomerController {  // /customer endpoint
 
 	@Autowired
-	private CustomerService CustomerService;  //customer interface
+	private CustomerService customerService;  //customer interface
 
 	@Autowired
-	AuthorizationFeign Authorizationfeign;   //auth feign
+	AuthorizationFeign authorizationFeign;   //auth feign
 
 	/**
 	 * @param token
